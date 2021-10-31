9a88c924df5bc8724d851b8e9dd30dfaa32075dc
Amanda Natallie <amanda.natallie.2@gmail.com>
2021-10-31 14:41:12 -0300

    Agora vai

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -10,9 +10,10 @@ import {
     colors,
     Divider,
     Dialog,
+    SearchSelect,
 } from '@vallorisolutions/foa-design-system';
 import { useFormik } from 'formik';
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 // import { api } from '../../api';
 import * as Yup from 'yup';
 import ReactLoading from 'react-loading';
@@ -39,6 +40,380 @@ const LoginPage: FC = (): JSX.Element => {
             children: null,
         },
     });
+
+    const [filteredClasses, setFilteredClasses] = useState<any[]>([]);
+    const [filteredEspecializations, setFilteredEspecializations] = useState<any[]>([]);
+
+    const raceOptions = [
+        {
+            value: 'ORC',
+            label: 'Orc',
+        },
+        {
+            value: 'UNDEAD',
+            label: 'Morto-Vivo / Undead',
+        },
+        {
+            value: 'TAUREN',
+            label: 'Tauren',
+        },
+        {
+            value: 'TROLL',
+            label: 'Troll',
+        },
+        {
+            value: 'GOBLIN',
+            label: 'Goblin',
+        },
+        {
+            value: 'BLOOD ELF',
+            label: 'Elfo Sangrento / Blood Elf',
+        },
+        {
+            value: 'PANDAREN',
+            label: 'Pandaren',
+        },
+        {
+            value: 'NIGHTBORNE',
+            label: 'Filho da Noite / Nightborne',
+        },
+        {
+            value: 'HIGHMOUNTAIN TAUREN',
+            label: 'Tauren Altamontês / Highmountain Tauren',
+        },
+        {
+            value: `ORC MAG'HAR`,
+            label: `Orc Mag'har`,
+        },
+        {
+            value: `ZANDALARI TROLL`,
+            label: `Zandalari Troll`,
+        },
+        {
+            value: 'VULPERA',
+            label: 'Vulpera',
+        },
+    ];
+
+    const availableClasses = [
+        {
+            value: 'DEATH KNIGHT',
+            label: 'Cavaleiro da Morte / Death Knight',
+            races: [
+                'ORC',
+                'UNDEAD',
+                'TAUREN',
+                'TROLL',
+                'GOBLIN',
+                'BLOOD ELF',
+                'PANDAREN',
+                'NIGHTBORNE',
+                'HIGHMOUNTAIN TAUREN',
+                "ORC MAG'HAR",
+                'ZANDALARI TROLL',
+                'VULPERA',
+            ],
+        },
+        {
+            value: 'DEMON HUNTER',
+            label: 'Caçador de Demônios / Demon Hunter',
+            races: ['BLOOD ELF'],
+        },
+        {
+            value: 'DRUID',
+            label: 'Druida / Druid',
+            races: ['HIGHMOUNTAIN TAUREN', 'TAUREN', 'TROLL', 'ZANDALARI TROLL'],
+        },
+        {
+            value: 'HUNTER',
+            label: 'Caçador / Hunter',
+            races: [
+                'ORC',
+                'UNDEAD',
+                'TAUREN',
+                'TROLL',
+                'GOBLIN',
+                'BLOOD ELF',
+                'PANDAREN',
+                'NIGHTBORNE',
+                'HIGHMOUNTAIN TAUREN',
+                "ORC MAG'HAR",
+                'ZANDALARI TROLL',
+                'VULPERA',
+            ],
+        },
+        {
+            value: 'MAGE',
+            label: 'Mago / Mage',
+            races: raceOptions
+                .filter((e) => e.value !== 'TAUREN' && e.value !== 'HIGHMOUNTAIN TAUREN')
+                .map((e) => e.value),
+        },
+        {
+            value: 'MONK',
+            label: 'Monge / Monk',
+            races: raceOptions.filter((e) => e.value !== 'GOBLIN').map((e) => e.value),
+        },
+        {
+            value: 'PALADIN',
+            label: 'Paladino / Paladin',
+            races: ['TAUREN', 'ZANDALARI TROLL', 'BLOOD ELF'],
+        },
+        {
+            value: 'PRIEST',
+            label: 'Sacerdote / Priest',
+            races: raceOptions
+                .filter((e) => e.value !== 'ORC' && e.value !== 'HIGHMOUNTAIN TAUREN')
+                .map((e) => e.value),
+        },
+        {
+            value: 'ROGUE',
+            label: 'Ladino / Rogue',
+            races: raceOptions
+                .filter((e) => e.value !== 'TAUREN' && e.value !== 'HIGHMOUNTAIN TAUREN')
+                .map((e) => e.value),
+        },
+        {
+            value: 'SHAMAN',
+            label: 'Xamã / Shaman',
+            RACES: raceOptions
+                .filter((e) => e.value !== 'BLOOD ELF' && e.value !== 'UNDEAD' && e.value !== 'NIGHTBORNE')
+                .map((e) => e.value),
+        },
+        {
+            value: 'WARLOCK',
+            label: 'Bruxo / Warlock',
+            races: raceOptions
+                .filter(
+                    (e) =>
+                        e.value !== 'TAUREN' &&
+                        e.value !== 'HIGHMOUNTAIN TAUREN' &&
+                        e.value !== `ORC MAG'HAR` &&
+                        e.value !== 'ZANDALARI TROLL' &&
+                        e.value !== 'PANDAREN',
+                )
+                .map((e) => e.value),
+        },
+        {
+            value: 'WARRIOR',
+            label: 'Guerreiro / Warrior',
+            races: raceOptions.map((e) => e.value),
+        },
+    ];
+
+    const availableEspecializations = [
+        {
+            value: 'ARMS',
+            label: 'Arms / Armas',
+            class: 'WARRIOR',
+        },
+        {
+            value: 'FURY',
+            label: 'Fury / Furia',
+            class: 'WARRIOR',
+        },
+        {
+            value: 'PROTECTION',
+            label: 'Protection / Proteção',
+            class: 'WARRIOR',
+        },
+        {
+            value: 'BEASTMASTERY',
+            label: 'Beast Mastery / Domínio das Feras',
+            class: 'HUNTER',
+        },
+        {
+            value: 'MARKSMANSHIP',
+            label: 'Marksmanship / Precisão',
+            class: 'HUNTER',
+        },
+        {
+            value: 'SURVIVAL',
+            label: 'Survival / Sobrevivência',
+            class: 'HUNTER',
+        },
+        {
+            value: 'ARCANE',
+            label: 'Arcane / Arcano',
+            class: 'MAGE',
+        },
+        {
+            value: 'FIRE',
+            label: 'Fire / Fogo',
+            class: 'MAGE',
+        },
+        {
+            value: 'FROST',
+            label: 'Frost / Gelo',
+            class: 'MAGE',
+        },
+        {
+            value: 'HOLY',
+            label: 'Holy / Sagrado',
+            class: 'PALADIN',
+        },
+        {
+            value: 'PROTECTION',
+            label: 'Protection / Proteção',
+            class: 'PALADIN',
+        },
+        {
+            value: 'RETRIBUTION',
+            label: 'Retribution / Reputação',
+            class: 'PALADIN',
+        },
+        {
+            value: 'DISCIPLINE',
+            label: 'Discipline / Disciplina',
+            class: 'PRIEST',
+        },
+        {
+            value: 'HOLY',
+            label: 'Holy / Sagrado',
+            class: 'PRIEST',
+        },
+        {
+            value: 'SHADOW',
+            label: 'Shadow / Sombra',
+            class: 'PRIEST',
+        },
+        {
+            value: 'ASSASSINATION',
+            label: 'Assassination / Assassinato',
+            class: 'ROGUE',
+        },
+        {
+            value: 'OUTLAW',
+            label: 'Outlaw / Fora da Lei',
+            class: 'ROGUE',
+        },
+        {
+            value: 'SUBTLETY',
+            label: 'Subtlety / Subterfugio',
+            class: 'ROGUE',
+        },
+        {
+            value: 'BREWMASTER',
+            label: 'Brewmaster / Mestre Cervejeiro',
+            class: 'MONK',
+        },
+        {
+            value: 'MISTWEAVER',
+            label: 'Mistweaver / Mistweaver',
+            class: 'MONK',
+        },
+        {
+            value: 'WINDWALKER',
+            label: 'Windwalker / Andarilho do Vento',
+            class: 'MONK',
+        },
+        {
+            value: 'BALANCE',
+            label: 'Balance / Equilíbrio',
+            class: 'DRUID',
+        },
+        {
+            value: 'FERAL',
+            label: 'Feral / Feral',
+            class: 'DRUID',
+        },
+        {
+            value: 'GUARDIAN',
+            label: 'Guardian / Guardião',
+            class: 'DRUID',
+        },
+        {
+            value: 'RESTORATION',
+            label: 'Restoration / Restauração',
+            class: 'DRUID',
+        },
+        {
+            value: 'DEMONOLOGY',
+            label: 'Demonology / Demonologia',
+            class: 'WARLOCK',
+        },
+        {
+            value: 'DESTRUCTION',
+            label: 'Destruction / Destruição',
+            class: 'WARLOCK',
+        },
+        {
+            value: 'AFFLICTION',
+            label: 'Affliction / Suplício',
+            class: 'WARLOCK',
+        },
+        {
+            value: 'HAVOC',
+            label: 'Havoc / Devastação',
+            class: 'DEMON HUNTER',
+        },
+        {
+            value: 'VENGEANCE',
+            label: 'Vengeance / Vingança',
+            class: 'DEMON HUNTER',
+        },
+        {
+            value: 'ELEMENTAL',
+            label: 'Elemental / Elemental',
+            class: 'SHAMAN',
+        },
+        {
+            value: 'ENHANCEMENT',
+            label: 'Enhancement / Aperfeiçoamento',
+            class: 'SHAMAN',
+        },
+        {
+            value: 'RESTORATION',
+            label: 'Restoration / Restauração',
+            class: 'SHAMAN',
+        },
+        {
+            value: 'BLOOD',
+            label: 'Blood / Sangue',
+            class: 'DEATH KNIGHT',
+        },
+        {
+            value: 'FROST',
+            label: 'Frost / Gelo',
+            class: 'DEATH KNIGHT',
+        },
+        {
+            value: 'UNHOLY',
+            label: 'Unholy / Profano',
+            class: 'DEATH KNIGHT',
+        },
+    ];
+
+    const handleRaceChange = (e: any): void => {
+        formik.setFieldValue('class', null);
+        setFilteredClasses([]);
+        formik.setFieldValue('especialization', null);
+        setFilteredEspecializations([]);
+        if (e) {
+            const classList: any = availableClasses.filter((c) => c.races?.includes(e.value));
+            const aaa = classList.map((n: any) => {
+                return {
+                    label: n.label,
+                    value: n.value,
+                };
+            });
+            setFilteredClasses(aaa);
+            formik.setFieldValue('race', e.value);
+        } else {
+            formik.setFieldValue('race', '');
+        }
+    };
+    const handleClassChange = (e: any): void => {
+        formik.setFieldValue('especialization', null);
+        setFilteredEspecializations([]);
+        if (e) {
+            const espList: any = availableEspecializations.filter((c) => c.class === e.value);
+            setFilteredEspecializations(espList);
+            formik.setFieldValue('class', e.value);
+        } else {
+            formik.setFieldValue('class', '');
+        }
+    };
     const formik = useFormik({
         initialValues: {
             name: '',
@@ -58,10 +433,12 @@ const LoginPage: FC = (): JSX.Element => {
             availableHours: Yup.string().required('Escolha um'),
             class: Yup.string().required('Digite a classe do seu personagem'),
             especialization: Yup.string().required('Digite a especialização do seu personagem'),
-            race: Yup.string().required('Digite a raça do seu personagem'),
+            race: Yup.string().required('Escolha a raça do seu personagem'),
             whatsapp: Yup.string().required('Digite seu whatsapp'),
             ilv: Yup.string().required('Digite seu item level'),
         }),
+        validateOnBlur: true,
+        validateOnChange: false,
         onSubmit: async () => {
             setIsLoading(true);
             try {
@@ -89,7 +466,9 @@ const LoginPage: FC = (): JSX.Element => {
             setIsLoading(false);
         },
     });
-
+    useEffect(() => {
+        console.log('formik.values', formik.values);
+    }, [formik]);
     return (
         <>
             <Dialog {...dialogInfo} />
@@ -228,45 +607,57 @@ const LoginPage: FC = (): JSX.Element => {
                                 />
                             </Col>
                             <br />
-
                             <Col size={12}>
-                                <Input
-                                    label="Classe"
-                                    name="class"
-                                    type="text"
-                                    placeholder="ex.: Demon Hunter"
-                                    onChange={(e): any => formik.setFieldValue('class', e.target.value.toUpperCase())}
-                                    value={formik.values.class}
-                                    messageError={formik.errors.class}
-                                />
-                            </Col>
-                            <br />
-                            <Col size={12}>
-                                <Input
-                                    label="Especialização"
-                                    name="especialization"
-                                    type="text"
-                                    placeholder="ex.: Tank - Vengeance"
-                                    onChange={(e): any =>
-                                        formik.setFieldValue('especialization', e.target.value.toUpperCase())
-                                    }
-                                    value={formik.values.especialization}
-                                    messageError={formik.errors.especialization}
-                                />
-                            </Col>
-                            <br />
-                            <Col size={12}>
-                                <Input
+                                <SearchSelect
                                     label="Raça"
                                     name="race"
-                                    type="text"
-                                    placeholder="ex.: Troll Zandalari"
-                                    onChange={(e): any => formik.setFieldValue('race', e.target.value.toUpperCase())}
-                                    value={formik.values.race}
+                                    placeholder="-- escolha um --"
+                                    options={raceOptions}
+                                    selectedValue={formik.values.race}
+                                    setSelectedValue={(e): any => handleRaceChange(e)}
+                                    isLoading={isLoading}
                                     messageError={formik.errors.race}
                                 />
                             </Col>
                             <br />
+                            {formik.values.race && formik.values.race.length > 0 && (
+                                <>
+                                    <Col size={12}>
+                                        <SearchSelect
+                                            label="Classe"
+                                            name="class"
+                                            options={filteredClasses}
+                                            selectedValue={formik.values.class}
+                                            setSelectedValue={(e): any => handleClassChange(e)}
+                                            isLoading={isLoading}
+                                            placeholder="-- escolha um --"
+                                            messageError={formik.errors.class}
+                                            isDisabled={!formik.values.race}
+                                        />
+                                    </Col>
+                                    <br />
+                                </>
+                            )}
+                            {formik.values.class && formik.values.class.length > 0 && (
+                                <>
+                                    <Col size={12}>
+                                        <SearchSelect
+                                            label="Especialização"
+                                            name="especialization"
+                                            placeholder="-- escolha um --"
+                                            options={filteredEspecializations}
+                                            selectedValue={formik.values.especialization}
+                                            setSelectedValue={(e: any): any =>
+                                                e ? formik.setFieldValue('especialization', e.value) : undefined
+                                            }
+                                            isLoading={isLoading}
+                                            messageError={formik.errors.especialization}
+                                            isDisabled={!formik.values.class}
+                                        />
+                                    </Col>
+                                    <br />
+                                </>
+                            )}
                             <Col size={12}>
                                 <Input
                                     label="ILV do seu personagem"
